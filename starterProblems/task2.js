75bf8d93cb1ac5ac16554b971a79b1900c4fabac
Howard Luong <37283238+howardluong@users.noreply.github.com>
2018-07-05 00:02:32 -0400

    Delete task2.js

--- starterProblems/task2.js
+++
@@ -1,4 +0,0 @@
-//Access the title of the search input using the getElementById method then log it to the console
-//We could have also went with the querySelector route
-var titleOfInput = document.getElementById('lst-ib').title;
-console.log(titleOfInput)
