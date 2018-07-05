246baf430d5b4cb931e2bd04e9744c8c9edefe9a
Howard Luong <37283238+howardluong@users.noreply.github.com>
2018-07-05 00:02:42 -0400

    Delete task3.js

--- starterProblems/task3.js
+++
@@ -1,9 +0,0 @@
-jquery = require('jquery'); //Require jquery through npm
-
-//It is better to start off declaring jquery variables with the '$' sign.
-//This would be an indicator for everyone that this is a jquery variable and not
-//a javascript variable
-var $titleOfInput = $('#lst-ib').attr('title')
-//We can access the search input's title by using jquery's id selector
-//instead of going the document.getElementById/querySelector route.
-//We can get the value of the title attribute by using the attr() method.
