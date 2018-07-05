24f2e0a86e0e9858efd83332b27573a0a281744a
Howard Luong <37283238+howardluong@users.noreply.github.com>
2018-07-05 00:02:54 -0400

    Delete task7.js

--- starterProblems/task7.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.container').querySelectorAll('p')['1']
-//Select the class that contains the elements we want and dive deeper by chaining
-//the querySelectorAll to select all the 'p' tags. Selecting them all will allow
-//us to select the specific 'p' tag we want by it's index
